1c698fd71865d7508f915d600bada10d5c0fc89b
Jintae Song <w.jinttao@gmail.com>
2024-12-12 18:03:51 +0900

    Test: add script load test ver6

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
-function loadScript(data) {
+const WHITE_SPACE = 5;
+const { width: widthViewport, height: heightViewport } = window.visualViewport;
+let targetElement = null;
+let message = "";
+
+function loadScript() {
   console.log("@toast setup start");
   const {
-    name,
-    type,
     target_element_id,
     message_title,
     message_body,
-    message_button_color_code,
     background_opacity,
-  } = data;
-  const WHITE_SPACE = 5;
-  const { width: widthViewport, height: heightViewport } = window.visualViewport;
-  const target = document.querySelector(`#${target_element_id}`);
-  console.log("target_element_id", target);
-  if (!target) {
+  } = message;
+  targetElement = document.querySelector(`#${target_element_id}`);
+  console.log("target_element_id", targetElement);
+  if (!targetElement) {
     console.log("no target_element_id");
     return;
   }
@@ -23,18 +23,38 @@ function loadScript(data) {
     height: heightTarget,
     x: xTarget,
     y: yTarget,
-  } = target.getBoundingClientRect();
+  } = targetElement.getBoundingClientRect();
   const yTargetInLayout = Math.ceil(yTarget) - WHITE_SPACE;
 
   const overlay = window.document.createElement("div");
   overlay.id = "welcomeToastOverlay";
-  setOverlay(widthViewport, heightViewport, widthTarget, heightTarget, xTarget, yTargetInLayout);
+  setOverlay(widthViewport, heightViewport, widthTarget, heightTarget, xTarget, yTargetInLayout, background_opacity);
   document.body.appendChild(overlay);
 
   const popover = window.document.createElement("div");
   popover.id = "welcomeToastPopover";
   overlay.insertAdjacentElement("afterend", popover);
   setPopover();
+
+  function handleOverlayWindowResize() {
+    const {
+      width: widthTarget,
+      height: heightTarget,
+      x: xTarget,
+      y: yTarget,
+    } = targetElement.getBoundingClientRect();
+    const yTargetInLayout = Math.ceil(yTarget) - WHITE_SPACE;
+
+    return setOverlay(
+      widthViewport,
+      heightViewport,
+      widthTarget,
+      heightTarget,
+      xTarget,
+      yTargetInLayout,
+      background_opacity,
+    );
+  }
 
   function setPopover() {
     const xTargetInLayout = xTarget + widthTarget + WHITE_SPACE;
@@ -59,51 +79,11 @@ function loadScript(data) {
   }
 
   function handlePopoverWindowResize() {
-    const { width: widthTarget, x: xTarget, y: yTarget } = target.getBoundingClientRect();
+    const { width: widthTarget, x: xTarget, y: yTarget } = targetElement.getBoundingClientRect();
     const xTargetInLayout = xTarget + widthTarget + WHITE_SPACE;
     popover.style.top = `${yTarget}px`;
     popover.style.left = `${xTargetInLayout}px`;
     return;
-  }
-
-  function setOverlay(widthViewport, heigthViewport, widthTarget, heightTarget, xTarget, yTarget) {
-    overlay.innerHTML = `
-        <svg
-          viewBox="0 0 ${widthViewport} ${heigthViewport}"
-          xmlSpace="preserve"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          version="1.1"
-          preserveAspectRatio="xMinYMin slice"
-          style="fill-rule: evenodd; clip-rule: evenodd; stroke-linejoin: round; stroke-miterlimit: 2; z-index: 10000; position: fixed; top: 0px; left: 0px; width: 100%; height: 100%;"
-        >
-          <path
-            d="M${widthViewport},0L0,0L0,${heigthViewport}L${widthViewport},${heigthViewport}L${widthViewport},0Z M${xTarget},${yTarget} h${widthTarget} a5,5 0 0 1 5,5 v${heightTarget} a5,5 0 0 1 -5,5 h-${widthTarget} a5,5 0 0 1 -5,-5 v-${heightTarget} a5,5 0 0 1 5,-5 z"
-            style="fill: rgb(0, 0, 0); opacity: ${background_opacity / 100}; pointer-events: auto; cursor: auto;"
-          >
-          </path>
-        </svg>
-    `;
-    return;
-  }
-
-  function handleOverlayWindowResize() {
-    const { width: widthViewport, height: heightViewport } = window.visualViewport;
-    const {
-      width: widthTarget,
-      height: heightTarget,
-      x: xTarget,
-      y: yTarget,
-    } = target.getBoundingClientRect();
-    const yTargetInLayout = Math.ceil(yTarget) - WHITE_SPACE;
-
-    return setOverlay(
-      widthViewport,
-      heightViewport,
-      widthTarget,
-      heightTarget,
-      xTarget,
-      yTargetInLayout,
-    );
   }
 
   function handleRemovePopover(event) {
@@ -126,36 +106,57 @@ window.onload = function () {
 }
 
 window.addEventListener("message", (e) => {
-  console.log("@child received message:", e.data);
-  const overlay = document.querySelector("#welcomeToastOverlay");
+  message = e.data;
+  const overlay = document.getElementById("welcomeToastOverlay");
   if (!overlay) {
-    console.log("@new overlay!");
-    loadScript(e.data);
+    loadScript();
   } else {
-    console.log("@modify overlay!");
-    const popoverHeader = document.querySelector("#welcomeToastPopoverHeader");
-    const popoverDescription = document.querySelector("#welcomeToastPopoverDescription");
-    const popoverFooter = document.querySelector("#welcomeToastPopoverFooter");
+    const popoverHeader = document.getElementById("welcomeToastPopoverHeader");
+    const popoverDescription = document.getElementById("welcomeToastPopoverDescription");
+    const popoverFooter = document.getElementById("welcomeToastPopoverFooter");
 
     const {
-      name,
-      type,
-      target_element_id,
       message_title,
       message_body,
-      message_button_color_code,
       background_opacity,
-    } = e.data;
+    } = message;
+    const {
+      width: widthTarget,
+      height: heightTarget,
+      x: xTarget,
+      y: yTarget,
+    } = targetElement.getBoundingClientRect();
 
+    setOverlay(widthViewport, heightViewport, widthTarget, heightTarget, xTarget, yTargetInLayout, background_opacity);
     popoverHeader.innerHTML = `<span>${message_title}</span>`;
     popoverDescription.innerHTML = `<span>${message_body}</span>`;
     popoverFooter.innerHTML = `<span>${message_body}</span>`;
   }
 })
 
-console.log("window parent:", window.parent);
 window.addEventListener("click", (e) => {
   console.log("@ clicked e.target: ", e.target, "e.target_id:", e.target.id);
-  const target =JSON.parse(JSON.stringify(e.target.id));
+  const target = JSON.parse(JSON.stringify(e.target.id));
   window.parent.postMessage({target}, "*");
 })
+
+function setOverlay(widthViewport, heigthViewport, widthTarget, heightTarget, xTarget, yTarget, background_opacity) {
+  overlay.innerHTML = `
+      <svg
+        viewBox="0 0 ${widthViewport} ${heigthViewport}"
+        xmlSpace="preserve"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        version="1.1"
+        preserveAspectRatio="xMinYMin slice"
+        style="fill-rule: evenodd; clip-rule: evenodd; stroke-linejoin: round; stroke-miterlimit: 2; z-index: 10000; position: fixed; top: 0px; left: 0px; width: 100%; height: 100%;"
+      >
+        <path
+          id="overlayPath"
+          d="M${widthViewport},0L0,0L0,${heigthViewport}L${widthViewport},${heigthViewport}L${widthViewport},0Z M${xTarget},${yTarget} h${widthTarget} a5,5 0 0 1 5,5 v${heightTarget} a5,5 0 0 1 -5,5 h-${widthTarget} a5,5 0 0 1 -5,-5 v-${heightTarget} a5,5 0 0 1 5,-5 z"
+          style="fill: rgb(0, 0, 0); opacity: ${background_opacity / 100}; pointer-events: auto; cursor: auto;"
+        >
+        </path>
+      </svg>
+  `;
+  return;
+}
