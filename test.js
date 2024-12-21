7b522d911b40cee44410b8f4039f5effff6d80b3
Jintae Song <w.jinttao@gmail.com>
2024-12-21 21:15:52 +0900

    Test: add script test ver19 (more toasts)

--- test.js
+++ test.js
@@ -6,14 +6,15 @@ s.defer = true;
 s.src = "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2";
 document.head.appendChild(s);
 
-const TARGET_ORIGIN = "http://localhost:5173";
+const TARGET_ORIGIN = "http://localhost:5173/";
 const SUPABASE_URL = "https://mepmumyanfvgmvjfjpld.supabase.co";
 const SUPABASE_API_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lcG11bXlhbmZ2Z212amZqcGxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM1Nzg2MDUsImV4cCI6MjA0OTE1NDYwNX0.HojnVr-YfuBy25jf9qy5DKYkqvdowZ0Pz2FScfIN-04";
 const WHITE_SPACE = 5;
 let overlay = null;
 let targetElement = null;
-let actionList = [];
+let toastList = [];
+let countToastClicked = 0;
 let messageFromPreview = "";
 let client;
 
@@ -32,7 +33,7 @@ async function getProject() {
         throw new Error(error);
       }
 
-      getAction(resultProject[0].id);
+      getToastList(resultProject[0].id);
     }
   } catch (e) {
     console.log(
@@ -43,32 +44,34 @@ async function getProject() {
   return;
 }
 
-async function getAction(projectId) {
+async function getToastList(projectId) {
   try {
     if (projectId) {
-      const { data: resultAction, error } = await client
-        .from("action")
+      const INDEX_FIRST_TOAST = 0;
+      const { data: resultToastList, error } = await client
+        .from("toast")
         .select("*")
         .eq("project_id", projectId);
 
-      if (resultAction.length === 0) {
+      if (resultToastList.length === 0) {
         throw new Error(error);
       }
 
-      actionList = [...resultAction];
-      applyAction();
+      toastList = [...resultToastList];
+      applyToast(INDEX_FIRST_TOAST);
     }
   } catch (e) {
     console.log(
-      "등록된 액션이 없습니다. @welcome-toast 관리자 페이지에서 액션 설정을 확인해주세요.",
+      "등록된 토스트가 없습니다. @welcome-toast 관리자 페이지에서 토스트 설정을 확인해주세요.",
     );
     console.error(e);
   }
   return;
 }
 
-function applyAction() {
-  const { target_element_id, message_title, message_body, background_opacity } = actionList[0];
+function applyToast(indexToast) {
+  const { target_element_id, message_title, message_body, image_url, background_opacity } =
+    toastList[indexToast];
   targetElement = document.querySelector(`#${target_element_id}`);
 
   if (!target_element_id || !targetElement) {
@@ -93,15 +96,19 @@ function applyAction() {
   );
 
   createPopover();
-  setPopover(targetElement, message_title, message_body);
+  setPopover(targetElement, message_title, message_body, image_url);
 
-  window.addEventListener("resize", handleOverlayWindowResize);
-  window.addEventListener("resize", handlePopoverWindowResize);
-  window.addEventListener("click", (event) => handleRemovePopover(event));
+  window.addEventListener("resize", handleOverlayWindowResizeScroll);
+  window.addEventListener("resize", handlePopoverWindowResizeScroll);
+  window.addEventListener("scroll", handleOverlayWindowResizeScroll);
+  window.addEventListener("scroll", handlePopoverWindowResizeScroll);
+  window.addEventListener("click", handleRemoveToast);
+  window.addEventListener("touchend", handleRemoveToast);
 }
 
-function applyActionAdminPreview() {
-  const { target_element_id, message_title, message_body, background_opacity } = messageFromPreview;
+function applyToastAdminPreview() {
+  const { target_element_id, message_title, message_body, image_url, background_opacity } =
+    messageFromPreview;
   targetElement = document.querySelector(`#${target_element_id}`);
 
   if (!target_element_id || !targetElement) {
@@ -134,11 +141,13 @@ function applyActionAdminPreview() {
     createPopover();
   }
 
-  setPopover(targetElement, message_title, message_body);
+  setPopover(targetElement, message_title, message_body, image_url);
 
-  window.addEventListener("resize", handleOverlayWindowResize);
-  window.addEventListener("resize", handlePopoverWindowResize);
-  window.addEventListener("click", handleRemovePopover);
+  window.addEventListener("resize", handleOverlayWindowResizeScroll);
+  window.addEventListener("resize", handlePopoverWindowResizeScroll);
+  window.addEventListener("scroll", handleOverlayWindowResizeScroll);
+  window.addEventListener("scroll", handlePopoverWindowResizeScroll);
+  window.addEventListener("click", handleRemoveToast);
 }
 
 function getWindowAndTargetSizePosition(targetElement) {
@@ -148,11 +157,13 @@ function getWindowAndTargetSizePosition(targetElement) {
     height: heightTarget,
     x: xTarget,
     y: yTarget,
+    right,
+    bottom,
   } = targetElement.getBoundingClientRect();
 
   return {
     window: { widthViewport, heightViewport },
-    target: { widthTarget, heightTarget, xTarget, yTarget },
+    target: { widthTarget, heightTarget, xTarget, yTarget, right, bottom },
   };
 }
 
@@ -196,41 +207,79 @@ function createPopover() {
   const overlay = document.querySelector("#welcomeToastOverlay");
 
   const popover = window.document.createElement("div");
+  const popoverImage = window.document.createElement("div");
   const popoverHeader = window.document.createElement("div");
   const popoverDescription = window.document.createElement("div");
   const popoverFooter = window.document.createElement("div");
 
   popover.id = "welcomeToastPopover";
+  popoverImage.id = "welcomeToastPopoverImage";
   popoverHeader.id = "welcomeToastPopoverHeader";
   popoverDescription.id = "welcomeToastPopoverDescription";
   popoverFooter.id = "welcomeToastPopoverFooter";
 
   overlay.insertAdjacentElement("afterend", popover);
+  popover.appendChild(popoverImage);
   popover.appendChild(popoverHeader);
   popover.appendChild(popoverDescription);
   popover.appendChild(popoverFooter);
   return;
 }
 
-function setPopover(targetElement, message_title, message_body) {
+function setPopover(targetElement, message_title, message_body, image_url) {
   const popover = document.querySelector("#welcomeToastPopover");
+  const popoverImage = document.querySelector("#welcomeToastPopoverImage");
   const popoverHeader = document.querySelector("#welcomeToastPopoverHeader");
   const popoverDescription = document.querySelector("#welcomeToastPopoverDescription");
   const popoverFooter = document.querySelector("#welcomeToastPopoverFooter");
 
-  const { target: t } = getWindowAndTargetSizePosition(targetElement);
+  const { window: w, target: t } = getWindowAndTargetSizePosition(targetElement);
+  const gapRight = w.widthViewport - (t.right + t.widthTarget);
   const xTargetInLayout = t.xTarget + t.widthTarget + WHITE_SPACE;
 
-  popoverHeader.innerHTML = `<span>${message_title}</span>`;
+  popoverHeader.innerHTML = `<span style="font-weight: bold;">${message_title}</span>`;
   popoverDescription.innerHTML = `<span>${message_body}</span>`;
-  popoverFooter.innerHTML = `<span>${message_body}</span>`;
+  popoverFooter.innerHTML = `<div style="width: 60%"></div><button id="welcomeToastPopoverButton" type="button">확인</button>`;
 
-  popover.style = `position: absolute; top: ${t.yTarget}px; left: ${xTargetInLayout}px; flex: auto; flex-direction: column; gap: 100px; padding: 15px; margin: 5px; border-radius: 5%; background: #242424; color: white; box-shadow: 0 1px 10px #0006; z-index: 1000000`;
+  if (image_url) {
+    popoverImage.innerHTML = `<img src=${image_url} alt="popoverFooter" width="100%" />`;
+  }
+
+  welcomeToastPopoverButton.addEventListener("click", handleToastButtonClick);
+
+  if (gapRight < 300) {
+    popover.style = `position: absolute; top: ${t.yTarget + t.heightTarget + WHITE_SPACE + window.scrollY}px; right: ${w.widthViewport - t.xTarget - t.widthTarget - WHITE_SPACE}px; flex: auto; flex-direction: column; max-height: 250px; min-width: 200px; max-width: 250px; padding: 15px; border: 1px; margin: 5px; border-radius: 5%; background: #242424; color: white; box-shadow: 0 1px 10px #0006; z-index: 1000000; overflow: clip; overflow-wrap: break-word; word-break: break-all;`;
+    popoverHeader.style = "margin-bottom: 5px;";
+    popoverDescription.style = "margin-bottom: 5px;";
+    popoverFooter.style = "display: flex; align-items: center; justify-content: space-between;";
+    return;
+  }
+
+  popover.style = `position: absolute; top: ${t.yTarget}px; left: ${xTargetInLayout}px; flex: auto; flex-direction: column; max-height: 250px; min-width: 200px; max-width: 250px; padding: 15px; border: 1px; margin: 5px; border-radius: 5%; background: #242424; color: white; box-shadow: 0 1px 10px #0006; z-index: 1000000; overflow: clip; overflow-wrap: break-word; word-break: break-all;`;
+  popoverHeader.style = "margin-bottom: 10px;";
+  popoverDescription.style = "margin-bottom: 10px;";
+  popoverFooter.style = "display: flex; align-items: center; justify-content: space-between;";
   return;
 }
 
-function handleOverlayWindowResize() {
-  const { target_element_id, background_opacity } = actionList[0];
+function handleToastButtonClick() {
+  const overlay = document.querySelector("#welcomeToastOverlay");
+  const popover = document.querySelector("#welcomeToastPopover");
+
+  overlay.remove();
+  popover.remove();
+
+  if (countToastClicked === toastList.length - 1) {
+    return;
+  }
+
+  countToastClicked += 1;
+  applyToast(countToastClicked);
+  return;
+}
+
+function handleOverlayWindowResizeScroll() {
+  const { target_element_id, background_opacity } = toastList[0];
   const targetElement = document.querySelector(`#${target_element_id}`);
   const { window: w, target: t } = getWindowAndTargetSizePosition(targetElement);
   const yTargetInLayout = Math.ceil(t.yTarget) - WHITE_SPACE;
@@ -247,21 +296,31 @@ function handleOverlayWindowResize() {
   return;
 }
 
-function handlePopoverWindowResize() {
-  const { target_element_id } = actionList[0];
+function handlePopoverWindowResizeScroll() {
+  const { target_element_id } = toastList[0];
   const targetElement = document.querySelector(`#${target_element_id}`);
-  const { target: t } = getWindowAndTargetSizePosition(targetElement);
-  const xTargetInLayout = t.xTarget + t.widthTarget + WHITE_SPACE;
   const popover = document.querySelector("#welcomeToastPopover");
+  const { window: w, target: t } = getWindowAndTargetSizePosition(targetElement);
+  const gapRight = w.widthViewport - (t.right + t.widthTarget);
 
-  if (popover) {
-    popover.style.top = `${t.yTarget}px`;
-    popover.style.left = `${xTargetInLayout}px`;
+  if (!popover) {
+    return;
   }
+
+  if (gapRight < 400) {
+    if (!window.scrollY) {
+      popover.style.top = `${t.bottom + WHITE_SPACE}px`;
+    }
+    popover.style.left = `${t.xTarget - WHITE_SPACE}px`;
+    return;
+  }
+
+  popover.style.top = `${t.yTarget}px`;
+  popover.style.left = `${t.right + WHITE_SPACE}px`;
   return;
 }
 
-function handleRemovePopover(event) {
+function handleRemoveToast(event) {
   const overlay = document.querySelector("#welcomeToastOverlay");
   const popover = document.querySelector("#welcomeToastPopover");
 
@@ -280,12 +339,10 @@ function handleMessageParent(event) {
 
 window.addEventListener("load", getProject);
 window.addEventListener("message", (event) => {
-  if (event.data.source) {
-    return;
+  if (event.origin === TARGET_ORIGIN) {
+    messageFromPreview = event.data;
+    applyToastAdminPreview();
   }
-
-  messageFromPreview = event.data;
-
-  applyActionAdminPreview();
+  return;
 });
 window.addEventListener("click", handleMessageParent);
